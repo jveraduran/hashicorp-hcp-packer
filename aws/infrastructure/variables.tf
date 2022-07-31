06494e7ca699abd8f34bafb7fa9e4070dc380cc7
jveraduran <51175225+jveraduran@users.noreply.github.com>
2022-07-31 11:17:50 -0400

    feat: Enable Terraform

--- aws/infrastructure/variables.tf
+++ aws/infrastructure/variables.tf
@@ -1,23 +1,19 @@
 variable "aws_access_key" {
   type        = string
-  default     = "AKIATUMEPLTIRYMJ5M77"
   description = "AWS access key"
 }
 
 variable "aws_secret_key" {
   type        = string
-  default     = "pVDwLitnu5gaonoCuYKQNRt5RAb8pr87U5UCjnyR"
   description = "AWS secret key"
 }
 
 variable "hcp_client_id" {
   type        = string
-  default     = "UDQpHCRav96bS8jGFp25goAAYV6Yzxng"
   description = "HCP Client ID"
 }
 
 variable "hcp_client_secret" {
   type        = string
-  default     = "lNyGq3njoGv6zwnXdkEB-UAB7FP9az4bha3J7Vj7_L4V78PUOGpt60XXiCG9cbgm"
   description = "HCP Client Secret"
 }
