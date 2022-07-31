f5a633803384fb5659c62b67110c7942a0cd2835
jveraduran <51175225+jveraduran@users.noreply.github.com>
2022-07-31 16:17:19 -0400

    feat: Enable Re-usable WF

--- aws/infrastructure/main.tf
+++ aws/infrastructure/main.tf
@@ -1,10 +1,10 @@
 data "hcp_packer_iteration" "ubuntu" {
-  bucket_name = "jveraduran"
+  bucket_name = "aws"
   channel     = "production"
 }
 
 data "hcp_packer_image" "ubuntu" {
-  bucket_name    = "jveraduran"
+  bucket_name    = "aws"
   cloud_provider = "aws"
   iteration_id   = data.hcp_packer_iteration.ubuntu.ulid
   region         = "us-east-1"
